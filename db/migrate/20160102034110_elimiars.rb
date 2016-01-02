31e8505690cb5af38884b039b10535f27e55aa98
Ignacio Norambuena <inorambuenaa@gmail.com>
2016-01-02 00:56:21 -0300

    Valoracion Corregida 4

--- db/migrate/20160102034110_elimiars.rb
+++
@@ -1,8 +0,0 @@
-class Elimiars < ActiveRecord::Migration
-  def change
-  	drop_table :rating_caches
-  	drop_table :rates
-  	drop_table :average_caches
-  	drop_table :overall_averages
-  end
-end
